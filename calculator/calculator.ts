30217be50f57129227d3f21b34edc736f07ebb85
Velislav21 <zirovcho34@gmail.com>
2025-03-19 11:11:43 +0200

    adding custom type alias

---
+++ calculator/calculator.ts
@@ -0,0 +1,20 @@
+type InvestmentData = {
+    initialAmmount: number,
+    annualContribution: number,
+    expectedReturn: number,
+    duration: number;
+};
+
+
+
+function calculateInvestment(data: InvestmentData) {
+
+}
+
+// function printResults(results) {
+
+// }
+
+// const results = calculateInvestment(...);
+
+// printResults(results) 
