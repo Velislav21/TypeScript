99653edb2377cb97b80e44a0cd56f6310dba6845
Velislav21 <zirovcho34@gmail.com>
2025-04-11 09:58:54 +0300

    method decorator

--- decorators/decorator.ts
+++ decorators/decorator.ts
@@ -1,21 +1,41 @@
-function logger<T extends new (...args: any[]) => any>(target: T, ctx: ClassDecoratorContext) {
-    console.log('logger decorator')
-    console.log(target)
-    console.log(ctx)
+function logger<T extends new (...args: any[]) => any>(
+    target: T,
+    ctx: ClassDecoratorContext
+) {
+    console.log("logger decorator");
+    console.log(target);
+    console.log(ctx);
 
     return class extends target {
-        age = 35;
+        constructor(...args: any[]) {
+            super(...args);
+
+            console.log("class construcor");
+            console.log(this);
+        }
     };
+}
+
+function autobind(
+    target: (...args: any[]) => any,
+    ctx: ClassMethodDecoratorContext
+) {
+    console.log('works')
+    ctx.addInitializer(function(this: any) {
+        this[ctx.name] = this[ctx.name].bind(this);
+    });
 }
 
 @logger
 class Person {
     name = "Max";
 
+    @autobind
     greet() {
-        console.log(`I am, ${this.name}`)
+        console.log(`I am ${this.name}`);
     }
-};
+}
 
 const max = new Person();
-console.log(max);
+const greet = max.greet;
+greet();
