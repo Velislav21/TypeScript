4ebbc58cf8b8195b27f19a6cfecabc635f0e6652
Velislav21 <zirovcho34@gmail.com>
2025-04-11 08:51:34 +0300

    decorator that edits a class

---
+++ decorators/decorator.ts
@@ -0,0 +1,21 @@
+function logger<T extends new (...args: any[]) => any>(target: T, ctx: ClassDecoratorContext) {
+    console.log('logger decorator')
+    console.log(target)
+    console.log(ctx)
+
+    return class extends target {
+        age = 35;
+    };
+}
+
+@logger
+class Person {
+    name = "Max";
+
+    greet() {
+        console.log(`I am, ${this.name}`)
+    }
+};
+
+const max = new Person();
+console.log(max);
