8313c041f06c8cb623c3c6b9fc81fc1e71f5cb6a
Velislav21 <zirovcho34@gmail.com>
2025-04-12 18:25:28 +0300

    adding basic functionality

---
+++ node-ts/data.ts
@@ -0,0 +1,34 @@
+interface Todo {
+    id: number;
+    text: string;
+}
+
+let TODOS: Todo[] = [];
+
+export function addTodo(text: string) {
+    const newTodo = { id: Math.random(), text };
+    TODOS.push(newTodo);
+    return newTodo;
+}
+
+export function getTodo(id: number) {
+    const todo = TODOS.find((todo) => todo.id === id);
+    if (!todo) {
+        throw new Error('Not Found!');
+    }
+    return todo;
+}
+
+export function getTodos() {
+    return TODOS;
+}
+
+export function removeTodo(id: number) {
+    TODOS = TODOS.filter((todo) => todo.id !== id);
+}
+
+export function updateTodo(id: number, text: string) {
+    const todo = getTodo(id);
+    todo.text = text;
+    return todo;
+}
