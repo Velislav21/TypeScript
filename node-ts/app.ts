d843e5b34692956bbdceca0dc64d68302789d80d
Velislav21 <zirovcho34@gmail.com>
2025-04-12 18:17:53 +0300

    setting up basic express server for practicing some typescript

---
+++ node-ts/app.ts
@@ -0,0 +1,10 @@
+import express from "express";
+
+const app = express();
+
+app.get('/', (req, res) => {
+    console.log(req.method);
+    res.json({message: "it works"});
+});
+
+app.listen(3000)
