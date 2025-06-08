2099904743a5eab401da0e519ea0c7d962a03f33
Velislav21 <zirovcho34@gmail.com>
2025-06-08 13:39:55 +0300

    practice decorators

---
+++ decorators/logger.ts
@@ -0,0 +1,22 @@
+function log(target: any, propertyKey: string, descriptor: PropertyDescriptor) {
+  const originalMethod = descriptor.value;
+
+  descriptor.value = function (...args: any[]) {
+    console.log(`Calling ${propertyKey} with`, args);
+    const result = originalMethod.apply(this, args);
+    console.log(`Called ${propertyKey}, result:`, result);
+    return result;
+  };
+
+  return descriptor;
+}
+
+class Calculator {
+  @log
+  add(a: number, b: number): number {
+    return a + b;
+  }
+}
+
+const calc = new Calculator();
+calc.add(2, 3);
